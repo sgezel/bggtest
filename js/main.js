01783d91ebe604636a075bd8ad2da767c131f4af
Sandor <spleethoven@gmail.com>
2013-08-27 14:24:10 +0200

    aaa

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 var app = {
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -10,24 +6,12 @@ var app = {
 			alert(title ? (title + ": " + message) : message);
 		}
 	},
-    findByName: function() {
-        var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-    },
-
-    initialize: function() {
-        var self = this;
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
+	initialize: function() {
+		var self = this;
 		this.store = new MemoryStore(function() {
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+	}
 };
 
 app.initialize();
